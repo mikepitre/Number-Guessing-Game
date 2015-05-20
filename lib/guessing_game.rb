fb70c61b8161d99bb686f6604b9dfc0066284296
Mike Pitre <[michaelepitre@gmail.com]>
2015-05-20 16:39:44 -0500

    normal mode completed

--- lib/guessing_game.rb
+++ lib/guessing_game.rb
@@ -1,27 +1,68 @@
-class Guessing_Game
+class Guessing_game
 
   def initialize
-    number = rand(1..100)
-    guesses_left = 5
+    @number = rand(1..100)
+    @guesses_left = 5
+    @previous_guesses = []
   end
 
-  puts "I'm thinking of a number between 1 and 100, want to try and guess it? (yes or no) > "
-  response = gets.chomp.downcase
-  if response == "yes"
-    puts "Okay you've got 5 guesses, I'll let you know if you're too high or too low. What's your first guess? > "
-    guess = gets.chomp.to_i
+
+  def start
+    welcome
+    evaluate_guess
+  end
+
+
+  def welcome
+    puts "I'm thinking of a number 1-100, try and guess it. You've got 5 guesses before you lose."
+  end
 
 
   def evaluate_guess
-    if guess < number
-      guesses_left -= 1
+    guess = gets.chomp.to_i
+    if @previous_guesses.include?(guess)
+      puts "It's still not #{guess} since last time you guessed that. Give me a new number."
+      evaluate_guess
+    elsif (@guesses_left == 1) && (guess != @number)
+      puts "You're out of guesses! It was #{@number}!"
+      play_again?
+    elsif guess < @number
+      @guesses_left -= 1
+      @previous_guesses << guess
       puts "Too Low!"
-    elsif guess > number
+      ask_for_guess
+      evaluate_guess
+    elsif guess > @number
+      @guesses_left -= 1
+      @previous_guesses << guess
       puts "Too High!"
-      guesses_left -= 1
-    else
+      ask_for_guess
+      evaluate_guess
+    elsif guess == @number
       puts "You got it! Isn't that the most exciting thing ever!?"
+      play_again?
+    else
+      "I need a number."
+      evaluate_guess
+    end
+  end
+
+  def ask_for_guess
+    puts "You have #{@guesses_left} guess left. Try again."
+  end
+
+
+  def play_again?
+    puts "Would you like to play again? (yes or no)"
+    response = gets.chomp.downcase
+    if response == "yes"
+      game = Guessing_game.new
+      game.start
+    else
+      puts "Thanks for playing this riveting game!"
     end
   end
 end
-end
+
+game = Guessing_game.new
+game.start
