3abb625dfbb4cc8f96629783a27ddfb6aa2e36f9
Mike Pitre <[michaelepitre@gmail.com]>
2015-05-20 13:50:05 -0500

    added first prompt

--- lib/guessing_game.rb
+++ lib/guessing_game.rb
@@ -1,4 +1,4 @@
-class guessing_game
+class Guessing_Game
 
   def initialize
     number = rand(1..100)
@@ -6,30 +6,22 @@ class guessing_game
   end
 
   puts "I'm thinking of a number between 1 and 100, want to try and guess it? (yes or no) > "
-  want_to_play? = gets.chomp.downcase
-  if want_to_play == "yes"
+  response = gets.chomp.downcase
+  if response == "yes"
     puts "Okay you've got 5 guesses, I'll let you know if you're too high or too low. What's your first guess? > "
     guess = gets.chomp.to_i
 
 
-
-
-
-
-
-
-def evaluate_guess
-  if guess < number
-    guesses_left -= 1
-    puts "Too Low!"
-  elsif guess > number
-    puts "Too High!"
-    guesses_left -= 1
-  else
-    puts "You got it! Isn't that the most exciting thing ever!?"
+  def evaluate_guess
+    if guess < number
+      guesses_left -= 1
+      puts "Too Low!"
+    elsif guess > number
+      puts "Too High!"
+      guesses_left -= 1
+    else
+      puts "You got it! Isn't that the most exciting thing ever!?"
+    end
   end
 end
-
-
-
 end
